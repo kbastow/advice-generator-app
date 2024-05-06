a72488a9fc2a9bdbd8a38091ffd0609fb36b7f49
Kate <bastowkate@gmail.com>
2024-05-06 23:19:51 +1000

    Adding MUI and base styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import './App.css'
-import Button from '@mui/material/Button'
-import AdviceBox from './components/advice/advice_slip'
+import "./App.css";
+import Container from "@mui/material/Container";
+import AdviceBox from "./components/advice/advice_slip";
 
 function App() {
   return (
     <>
-      <AdviceBox></AdviceBox>
-      <Button variant="outlined">First button</Button>
+      <Container maxWidth="sm">
+        <AdviceBox></AdviceBox>
+        <button
+          style={{
+            background: "hsl(150, 100%, 66%)",
+            border: "none",
+            width: "44px",
+            height: "44px",
+            lineHeight: "44px",
+            borderRadius: "50%",
+            zIndex: "1",
+            position: "relative",
+            display: "block",
+            margin: "0 auto",
+            top: "-1.4rem"
+          }}
+        >
+          <img
+            className="center"
+            src="/src/assets/icon-dice.svg"
+            alt="dice icon"
+            style={{ maxWidth: "50%" }}
+          />
+        </button>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/advice/advice_slip.jsx
+++ src/components/advice/advice_slip.jsx
@@ -1,14 +1,16 @@
+import './advice_slip.css'
 import Box from '@mui/system/Box';
 
 export default function AdviceBox() {
   return (
     <Box component="section" sx={{ 
-        borderRadius: '1rem',
+        borderRadius: '8px',
         bgcolor: 'hsl(217, 19%, 24%)',
-        height: '10rem',
-        width: '100%'
+        padding: '3rem 2rem'
     }}>
-      This Box renders as an HTML section element.
+      <p className='advice-label'>ADVICE #{"123"}</p>
+      <p className='advice-content'>&quot;{"Generated advice placeholder lorem ipsum Generated advice placeholder lorem ipsum."}&quot;</p>
+      <img className='content-divider' src='/src/assets/pattern-divider-mobile.svg'></img>
     </Box>
   );
 }
