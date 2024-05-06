4fd41aeb5f315cab622fe8fb435bcd40add2f933
Kate <bastowkate@gmail.com>
2024-05-07 07:46:15 +1000

    create useState useEffect added to AdviceComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,12 @@
 import "./App.css";
 import Container from "@mui/material/Container";
-import AdviceBox from "./components/advice/advice_slip";
+import AdviceComponent from "./components/advice/advice_slip";
 
 function App() {
   return (
     <>
       <Container maxWidth="sm">
-        <AdviceBox></AdviceBox>
-        <button
-          style={{
-            background: "hsl(150, 100%, 66%)",
-            border: "none",
-            width: "44px",
-            height: "44px",
-            lineHeight: "44px",
-            borderRadius: "50%",
-            zIndex: "1",
-            position: "relative",
-            display: "block",
-            margin: "0 auto",
-            top: "-1.4rem"
-          }}
-        >
-          <img
-            className="center"
-            src="/src/assets/icon-dice.svg"
-            alt="dice icon"
-            style={{ maxWidth: "50%" }}
-          />
-        </button>
+        <AdviceComponent></AdviceComponent>
       </Container>
     </>
   );
--- src/components/advice/advice_slip.jsx
+++ src/components/advice/advice_slip.jsx
@@ -1,16 +1,69 @@
-import './advice_slip.css'
-import Box from '@mui/system/Box';
+import "./advice_slip.css";
+import Box from "@mui/system/Box";
+import { useState, useEffect } from "react";
 
-export default function AdviceBox() {
+const AdviceComponent = () => {
+  const [data, setData] = useState({
+    id: "123",
+    advice: "Generic placeholders can also offer words of advice.",
+  });
+
+  useEffect(() => {
+    fetch("https://api.adviceslip.com/advice")
+    .then((response) => response.json())
+    .then((data) => 
+    setData({
+      id: data.slip.id,
+      advice: data.slip.advice,
+    })
+  )
+  .catch((error) => console.error(error));
+
+  }, []);
+
   return (
-    <Box component="section" sx={{ 
-        borderRadius: '8px',
-        bgcolor: 'hsl(217, 19%, 24%)',
-        padding: '3rem 2rem'
-    }}>
-      <p className='advice-label'>ADVICE #{"123"}</p>
-      <p className='advice-content'>&quot;{"Generated advice placeholder lorem ipsum Generated advice placeholder lorem ipsum."}&quot;</p>
-      <img className='content-divider' src='/src/assets/pattern-divider-mobile.svg'></img>
+    <Box
+      component="section"
+      sx={{
+        borderRadius: "8px",
+        bgcolor: "hsl(217, 19%, 24%)",
+        padding: "3rem 2rem",
+      }}
+    >
+      <p className="advice-label">ADVICE #{data.id}</p>
+      <p className="advice-content">
+        &quot;
+        {data.advice}
+        &quot;
+      </p>
+      <img
+        className="content-divider"
+        src="/src/assets/pattern-divider-mobile.svg"
+      ></img>
+      <button
+        style={{
+          background: "hsl(150, 100%, 66%)",
+          border: "none",
+          width: "44px",
+          height: "44px",
+          lineHeight: "44px",
+          borderRadius: "50%",
+          zIndex: "1",
+          position: "relative",
+          display: "block",
+          margin: "0 auto",
+          bottom: "-4.5rem",
+        }}
+      >
+        <img
+          className="center"
+          src="/src/assets/icon-dice.svg"
+          alt="dice icon"
+          style={{ maxWidth: "50%" }}
+        />
+      </button>
     </Box>
   );
 }
+
+export default AdviceComponent;
