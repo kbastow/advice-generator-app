02436987cff9a8d509d961e21adff27c89d66bc6
Kate Bastow <kate.b@montu.com.au>
2024-05-27 12:21:14 +1000

    Refactor AdviceGenerator function to handle data fetching asynchronously

--- src/components/advice/advice_slip.jsx
+++ src/components/advice/advice_slip.jsx
@@ -2,29 +2,29 @@ import "./advice_slip.css";
 import Box from "@mui/system/Box";
 import { useState, useEffect } from "react";
 
-const AdviceComponent = () => {
+function AdviceGenerator() {
   const [data, setData] = useState({
     id: "123",
     advice: "Generating words of advice...",
   });
 
-  const [buttonClick, setButtonClick] = useState(0);
-
-  useEffect(() => {
-    fetch("https://api.adviceslip.com/advice")
-      .then((response) => response.json())
-      .then((data) =>
-        setData({
-          id: data.slip.id,
-          advice: data.slip.advice,
-        })
-      )
-      .catch((error) => console.error(error));
-  }, [buttonClick]);
-
-  const getAdvice = () => {
-    setButtonClick(buttonClick + 1);
+  const fetchAdvice = async () => {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const result = await response.json();
+      setData({
+        id: result.slip.id,
+        advice: result.slip.advice,
+      });
+    } catch (error) {
+      console.error(error);
+    }
   };
+
+  // Fetch advice on component mount
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
 
   return (
     <Box
@@ -36,32 +36,13 @@ const AdviceComponent = () => {
       }}
     >
       <p className="advice-label">ADVICE #{data.id}</p>
-      <p className="advice-content">
-        &quot;
-        {data.advice}
-        &quot;
-      </p>
+      <p className="advice-content">&quot;{data.advice}&quot;</p>
       <img
         className="content-divider"
         src="/src/assets/pattern-divider-mobile.svg"
-      ></img>
-      <button
-        onClick={getAdvice}
-        style={{
-          background: "hsl(150, 100%, 66%)",
-          border: "none",
-          width: "44px",
-          height: "44px",
-          lineHeight: "44px",
-          borderRadius: "50%",
-          zIndex: "1",
-          position: "relative",
-          display: "block",
-          margin: "0 auto",
-          bottom: "-4.5rem",
-          cursor: "pointer",
-        }}
-      >
+        alt="divider"
+      />
+      <button className="advice-button" onClick={fetchAdvice}>
         <img
           className="center"
           src="/src/assets/icon-dice.svg"
@@ -71,6 +52,6 @@ const AdviceComponent = () => {
       </button>
     </Box>
   );
-};
+}
 
-export default AdviceComponent;
+export default AdviceGenerator;
